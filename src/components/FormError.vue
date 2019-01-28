85eb35f15edf931532019c2518e744f927925792
Your Name <you@example.com>
2019-01-27 23:44:39 -0400

    save

---
+++ src/components/FormError.vue
@@ -0,0 +1,9 @@
+<template>
+    <span v-show="errors_form.has(attribute_name)" class="help brand-color">{{ errors_form.first(attribute_name) }}</span>
+</template>
+
+<script>
+    export default {
+        props: ['attribute_name', 'errors_form']
+    }
+</script>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -25,12 +25,26 @@
                                     </div>
                                 </div>
 								<div class="form-group m-form__group">
-									<input class="form-control m-input"   type="text" placeholder="Email" name="email" autocomplete="off" v-model.lazy="inputsData.email">
+									<input 	class="form-control m-input"
+											type="text" 
+											placeholder="Email" 
+											name="email"
+											autocomplete="off" 
+											v-model.lazy="inputsData.email"
+											v-validate="'required'"
+                							:class="{'has-error': errors.has('email')}">
+									<FormError :attribute_name="'email'" :errors_form="errors"> </FormError>
 								</div>
 								<div class="form-group m-form__group">
-									<input class="form-control m-input m-login__form-input--last" type="password" placeholder="Password" name="password" v-model.lazy="inputsData.password">
+									<input 	class="form-control m-input m-login__form-input--last" 
+											type="password" 
+											placeholder="Password" 
+											name="password" 
+											v-model.lazy="inputsData.password"
+											v-validate="'required'"
+											:class="{'has-error': errors.has('password')}">
 								</div>
-								
+								<FormError :attribute_name="'password'" :errors_form="errors"> </FormError>
 								<div class="m-login__form-action">
 									<button type="button" v-on:click="validLogin" id="m_login_signin_submit" class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn">
 										Sign In
@@ -47,12 +61,15 @@
 				</div>
 			</div>
 		</div>
-		<FormError :attribute_name="'firstname'" :errors_form="errors"> </FormError>
 	</body>
 </template>
 <script>
+	import FormError from './FormError';
     export default {
-        name: 'Login',
+		name: 'Login',
+		components: {
+            FormError
+        },
         props: {
             msg: String
         },
@@ -66,17 +83,20 @@
         },
         methods : {
             validLogin : function () {
-                this.axios.get('http://apilayer.net/api/historical?access_key=3da0e9a1d2bb1bee46cb1dc1b2302aa7&date=2018-09-19&source=USD&currencies=COP').then((response) => {
-                    this.resp = response.data;
-                     if(this.resp.quotes.USDCOP<=3030){
-                        //window.location.href = 'index';
-                        this.valid = true;
-                        this.message = 'Error'
-                        //this.$router.go('/index') 
-                    }else{
-                        this.valid = false;
+				this.$validator.validateAll().then((result) => {
+                    if (result) {
+                        this.axios.get('http://apilayer.net/api/historical?access_key=3da0e9a1d2bb1bee46cb1dc1b2302aa7&date=2018-09-19&source=USD&currencies=COP').then((response) => {
+							this.resp = response.data;
+							if(this.resp.quotes.USDCOP<=3030){
+								//window.location.href = 'index';
+								this.valid = true;
+								this.message = 'Error'
+								//this.$router.go('/index') 
+							}else{
+								this.valid = false;
+							}
+						});
                     }
-                    console.log(status,this.resp.quotes.USDCOP);
                 });
             }
         }
