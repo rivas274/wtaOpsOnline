030ab5228ef2762b0d83a079bed5352bc140ac3d
rivas274 <sr.naspter@gmail.com>
2019-05-03 16:00:40 -0400

    timeOut added

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ import VueSession from './custom/vue-session-custom';
 import routes from './custom/routes';
 import middleware from './custom/middleware';
 import permission from './custom/permission.json';
-import globalFilters from'./custom/vue-global-filters';
-import globalDirectives from'./custom/vue-global-directives';
+import globalFilters from './custom/vue-global-filters';
+import globalDirectives from './custom/vue-global-directives';
 import VueParticles from 'vue-particles';
 
 Vue.use(VueParticles)
@@ -18,74 +18,96 @@ Vue.use(globalDirectives);
 Vue.use(globalFilters);
 Vue.use(VeeValidate);
 Vue.use(VueSession, {
-  persist: true
+    persist: true
 });
-Vue.use(middleware,permission);
+Vue.use(middleware, permission);
 const router = new VueRouter({ mode: 'history', routes: routes });
 const customAxios = axios.create({
-  baseURL: 'https://wtaops.com/app/apiWtaOnline/',
-  headers: {
-    'Content-Type': 'multipart/form-data'
-  }
+    baseURL: 'https://wtaops.com/app/apiWtaOnline/',
+    headers: {
+        'Content-Type': 'multipart/form-data'
+    }
 });
 customAxios.interceptors.response.use(
-  function (response) {
-    return response;
-  },
-  function (error) {
-    if (error.response.status == 401 && router.currentRoute.fullPath!=='/') {
-      Vue._session.set('TOKEN','');
-      router.go('/');
+    function (response) {
+        Vue._session.set('lastCheck', moment().format('YYYY-MM-DD hh:mm A'));
+        Vue._session.set('checkTimeOut', false);
+        return response;
+    },
+    function (error) {
+        if (error.response.status == 401 && router.currentRoute.fullPath !== '/') {
+            Vue._session.set('TOKEN', '');
+            router.go('/');
+        }
+        return error.response;
     }
-    return error.response;
-  }
 );
 customAxios.interceptors.request.use(function (config) {
-  if(!Vue._session.exists()){
-    Vue._session.start();
-  }
-  config.headers.common['SESSION'] = Vue._session.id().replace('sess:','');
-  if ((Vue._session.get('TOKEN') || '').length == 16 && (config.headers.common['TOKEN'] || '').length == 0) {
-    config.headers.common['TOKEN'] = Vue._session.get('TOKEN');
-    config.headers.common['USER'] = Vue._session.get("user");
-  }
-  return config;
+    if (!Vue._session.exists()) {
+        Vue._session.start();
+    }
+    config.headers.common['SESSION'] = Vue._session.id().replace('sess:', '');
+    if ((Vue._session.get('TOKEN') || '').length == 16 && (config.headers.common['TOKEN'] || '').length == 0) {
+        config.headers.common['TOKEN'] = Vue._session.get('TOKEN');
+        config.headers.common['USER'] = Vue._session.get("user");
+    }
+    return config;
 }, function (err) {
-  return Promise.reject(err);
+    return Promise.reject(err);
 });
 Vue.mixin({
-  methods:{
-    baseUrlApi:function() {
-      return 'https://' + this.axios.defaults.baseURL.replace('http://', '')
-        .replace('https://', '')
-        .replace('www.', '')
-        .split(/[/?#]/)[0]+'/';
+    methods: {
+        baseUrlApi: function () {
+            return 'https://' + this.axios.defaults.baseURL.replace('http://', '')
+                .replace('https://', '')
+                .replace('www.', '')
+                .split(/[/?#]/)[0] + '/';
+        }
     }
-  }
 })
 Vue.use(VueAxios, customAxios);
 router.beforeEach((to, from, next) => {
-  //console.log('Vue.$canSee',Vue.$canSee([18]));
-  if (to.meta.isPublic) {
-    if (to.name == "Login" && (Vue._session.get('TOKEN') || '').length == 16) {
-      next('/dasboard');
+    //console.log('Vue.$canSee',Vue.$canSee([18]));
+    if (to.meta.isPublic) {
+        if (to.name == "Login" && (Vue._session.get('TOKEN') || '').length == 16) {
+            next('/dasboard');
+        } else {
+            next();
+        }
     } else {
-      next();
+        if (Vue._session.exists() && (Vue._session.get('TOKEN') || '').length == 16) {
+            if (Vue._session.get('changePassword') == 1 && to.name != 'MyAcount') {
+                next('MyAcount');
+            } else {
+                next();
+            }
+        } else {
+            next('/');
+        }
     }
-  } else {
-    if (Vue._session.exists() && (Vue._session.get('TOKEN') || '').length == 16) {
-      if (Vue._session.get('changePassword')==1 && to.name!='MyAcount') {
-        next('MyAcount');
-      } else {
-        next();
-      }
-    } else {
-      next('/');
-    }
-  }
 });
 Vue.config.productionTip = false;
 new Vue({
-  render: h => h(App),
-  router
+    mounted: function () {
+        setInterval(() =>{
+            if (router.currentRoute.fullPath !== '/') {
+                let lastCheck = moment(Vue._session.get('lastCheck')),
+                    check = Vue._session.get('checkTimeOut'),
+                    duration = moment.duration(moment().diff(lastCheck)).asMinutes();
+                if (duration >= 10) {
+                    this.axios.post("timeOut", { 'reset': check }).then(response => {
+                        if (response.data.STATUS == "OK") {
+                            Vue._session.set('lastCheck', moment().format('YYYY-MM-DD hh:mm A'));
+                            Vue._session.set('checkTimeOut', false);
+                        }
+                    });
+                }
+            }
+        }, (5 * 60 * 1000));
+        document.addEventListener("click", function () {
+            Vue._session.set('checkTimeOut', true);
+        });
+    },
+    render: h => h(App),
+    router
 }).$mount('#app')
