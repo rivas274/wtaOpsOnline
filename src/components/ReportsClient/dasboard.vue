943c94c05983500da8fa7123c17010b863095b31
Hrivas <hrivas@travelsupport.info>
2022-02-07 11:39:12 -0400

    correccion formulario reporte de clientes

--- src/components/ReportsClient/dasboard.vue
+++ src/components/ReportsClient/dasboard.vue
@@ -1,5 +1,5 @@
 <template>
-    <content-m :navigation="[{ title: $t('general.reports') }]">
+    <content-m :navigation="[{ title: $t('general.reports') }]"  :class="{'m-loader m-loader--metal m-loader--div':showLoader}">
         <template slot="title">{{ $t("general.reports") }}</template>
         <template slot="body">
             <h3 class="">
@@ -7,24 +7,41 @@
             </h3>
             <hr />
             <div class="row">
-                <div class="form-group m-form__group col-md-4">
+                <div class="form-group m-form__group col-md-4" :class="{'has-danger': errors.has('idReport')}">
                     <strong>{{ $t("general.reports") }}</strong>
                     <select-from-table
                         name="idReport"
+                        v-validate="'required'"
+                        :data-vv-as="$t('general.reports')"
                         :options="reportsAvailable"
                         :selected="filters.idReport"
                         v-on:input="setDataFilter"
                     ></select-from-table>
+                    <form-error
+                        :attribute_name="'idReport'"
+                        :errors_form="errors"
+                    ></form-error>
                 </div>
-                <div class="form-group m-form__group col-md-4">
+                <div class="form-group m-form__group col-md-4"  :class="{'has-danger': errors.has('startDate') ||  errors.has('endDate')}">
                     <strong>{{ $t("general.selectDateRange") }}</strong>
                     <date-range-bt
                         class="m-form__group-sub"
                         name="date"
+                        v-validate="'required'"
                         :watermark="$t('general.selectDateRange')"
                         v-on:input="setDataFilter"
                         :value="filters.date"
                     ></date-range-bt>
+                    <input  type="hidden"
+                            name="startDate"
+                            :data-vv-as="$t('general.dateRange')"
+                            :value="filters.date.startDate"
+                            v-validate="'required|date_format:yyyy-MM-dd'">
+                    <form-error
+                        class="mr-2"
+                        :attribute_name="'startDate'"
+                        :errors_form="errors"
+                    ></form-error>
                 </div>
                 <div class="col m--align-right">
                     <button
@@ -45,22 +62,26 @@
 import contentM from "../Content.vue";
 import dateRangeBt from "../Tables/filters/dateRangeBt.vue";
 import inputGroup from "../Tables/filters/inputGroup.vue";
-
+import FormError from "../FormError";
 import selectFromTable from "../Tables/filters/selectFromTable.vue";
 export default {
     components: {
         contentM,
         dateRangeBt,
         inputGroup,
-        selectFromTable
+        selectFromTable,
+        FormError
     },
     data() {
         return {
             reportsAvailable: [],
-            error: "",
+            showLoader:false,
             filters: {
                 idReport: null,
-                date: {}
+                date: {
+                    startDate:null,
+                    endDate:null
+                }
             }
         };
     },
@@ -77,7 +98,10 @@ export default {
         clear: function () {
             this.filters = {
                 idReport: null,
-                date: {}
+                date: {
+                    startDate:null,
+                    endDate:null
+                }
             };
         },
         getDocumentsType: function () {
@@ -85,6 +109,10 @@ export default {
                 id: this.$session.get("permission")['guard_by_client']['additional_data']
             }).then(response => {
                 this.reportsAvailable = response.data.RESPONSE.RESULTS;
+                if(this.reportsAvailable.length==1){
+                    this.filters.idReport=this.reportsAvailable[0].id;
+                }
+                
             });
         },
         dowload: function () {
@@ -92,43 +120,32 @@ export default {
                 idReport: this.filters.idReport,
                 endDate: this.filters.date.endDate,
                 startDate: this.filters.date.startDate
-            },valid = false;
-
-            for (var field in requiered) {
-                if (requiered[field] != "") {
-                    valid = true;
+            };
+            this.$validator.validateAll().then(result => {
+                if(result){
+                    this.showLoader = true;
+                    this.axios
+                        .post("guardByClientXLS",
+                            requiered,
+                            {
+                                responseType: "blob"
+                            }
+                        )
+                        .then(response => {
+                            const url = window.URL.createObjectURL(
+                                new Blob([response.data])
+                            );
+                            const link = document.createElement("a");
+                            link.href = url;
+                            link.setAttribute(
+                                "download", "guardByClientXLS" + (this.filters.idReport) + ".xlsx"
+                            );
+                            document.body.appendChild(link);
+                            link.click();
+                            this.showLoader = false;
+                        });
                 }
-            }
-
-            if (!valid) {
-                this.error = "Select a filter to continue";
-                return false;
-            } else {
-                this.error = null;
-            }
-
-            this.showLoader = true;
-            this.axios
-                .post("guardByClientXLS",
-                    requiered,
-                    {
-                        responseType: "blob"
-                    }
-                )
-                .then(response => {
-                    const url = window.URL.createObjectURL(
-                        new Blob([response.data])
-                    );
-                    const link = document.createElement("a");
-                    link.href = url;
-                    link.setAttribute(
-                        "download", "guardByClientXLS" + (this.filters.idReport) + ".xlsx"
-                    );
-                    document.body.appendChild(link);
-                    link.click();
-                    this.error = null;
-                    this.showLoader = false;
-                });
+            });
         },
     }
 };
